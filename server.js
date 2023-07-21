f2d0c446b302fa403495bb3b61ea091143d4dc65
mousab-issa <59908473+mousab-issa@users.noreply.github.com>
2023-07-21 16:27:58 +0700

    fix: fixing the origin

--- server.js
+++ server.js
@@ -38,11 +38,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://mental-health-dashboard.onrender.com/",
-      "*",
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
